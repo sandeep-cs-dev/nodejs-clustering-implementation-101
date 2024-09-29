b2df077286a6e1e3f3289368b43f4e11ee563f29
sandeep-cs-dev <sandeeppatelsgsits@gmail.com>
2024-09-29 16:22:34 +0200

    basic clustering demonstration using child_process

--- shared-handle/primary-shared-handle-policy.js
+++ shared-handle/primary-shared-handle-policy.js
@@ -6,10 +6,11 @@ const numCPUs = os.cpus().length;
 const workers = [];
 
 // Fork workers
-const op = {execArgv: ['--inspect-brk=0']};
+//const op = {execArgv: ['--inspect-brk=0']};
 
 for (let i = 1; i <=numCPUs;i++) {
-    const worker = fork('shared_handle_worker.js',op);
+    //const worker = fork('worker-shared-handle-policy.js',op);
+    const worker = fork('worker-shared-handle-policy.js');
     workers.push(worker);
 }
 const serverHandle = net._createServerHandle(null, 8080, 4,null,null);
